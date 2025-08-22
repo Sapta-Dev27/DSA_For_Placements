a099506116d9a4a153bc0df836b5d4fd5260f35c
Saptarshi Paul <saptarshi2027paul@gmail.com>
2025-08-22 22:30:06 +0530

    countSquares

---
+++ BinarySearch/countSquares.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter the value of n : ";
+  cin >> n;
+  int count = 0;
+  for (int i = 1; i < n; i++)
+  {
+    if (i * i == n)
+    {
+      break;
+    }
+    else if (i * i < n)
+    {
+      count++;
+    }
+    else
+    {
+      break;
+    }
+  }
+  cout << count << endl;
+  return 0;
+}
